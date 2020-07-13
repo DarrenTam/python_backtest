5bfd338983c1aaee9a12f9297ddb48918e045b82
Darren Tam <darrentamhk@gmail.com>
2020-07-13 23:32:58 +0800

    init

---
+++ performance.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import numpy as np 
+import pandas as pd
+def create_sharpe_ratio(returns, periods=252): 
+    return np.sqrt(periods) * (np.mean(returns)) / np.std(returns)
+def create_drawdowns(pnl):
+    hwm = [0]
+    idx = pnl.index
+    drawdown = pd.Series(index = idx) 
+    duration = pd.Series(index = idx)
+    for t in range(1, len(idx)):
+        hwm.append(max(hwm[t-1], pnl[t]))
+        drawdown[t]= (hwm[t]-pnl[t])
+        duration[t]= (0 if drawdown[t] == 0 else duration[t-1]+1)
+        return drawdown, drawdown.max(), duration.max()
